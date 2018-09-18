fe31ce6af2074b3653954bd59f772d0459ca5602
alien190 <alien1900@bk.ru>
2018-09-18 11:27:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/alien/recyclerviewdraddrop/RecyclerListFragment.java
@@ -0,0 +1,58 @@
+package com.example.alien.recyclerviewdraddrop;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.alien.recyclerviewdraddrop.helper.SimpleItemTouchHelperCallback;
+
+
+/**
+ * @author Paul Burke (ipaulpro)
+ */
+public class RecyclerListFragment extends Fragment {
+
+    private ItemTouchHelper mItemTouchHelper;
+
+    public static RecyclerListFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        RecyclerListFragment fragment = new RecyclerListFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public RecyclerListFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerListAdapter adapter = new RecyclerListAdapter();
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+}
---
+++ app/src/main/java/com/example/alien/recyclerviewdraddrop/common/SingleFragmentActivity.java
@@ -0,0 +1,41 @@
+package com.example.alien.recyclerviewdraddrop.common;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.alien.recyclerviewdraddrop.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_single_fragment);
+        if (savedInstanceState == null) {
+            changeFragment(getFragment());
+        }
+    }
+
+    protected abstract Fragment getFragment();
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.getBackStackEntryCount() == 1) {
+            finish();
+        } else {
+            fragmentManager.popBackStack();
+        }
+    }
+
+    protected void changeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .addToBackStack(fragment.getClass().getSimpleName())
+                .commit();
+    }
+}
